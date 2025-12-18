be953a8595b01dbf1e4c4bec7fa5f193a7b558dc
Sergey <sergey.meljnick@yandex.ru>
2025-12-18 13:18:34 +0300

    added stage 2 debug 2

--- k8s-ansible/provider.tf
+++ k8s-ansible/provider.tf
@@ -13,8 +13,8 @@ terraform {
     bucket                      = "diplom-tf-backend"
     region                      = "ru-central1"
     key                         = "terraform.tfstate"
-    access_key                  = "key"
-    secret_key                  = "key"
+    access_key                  = key
+    secret_key                  = key
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
